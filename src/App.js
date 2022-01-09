ad417c84845929b1298d2b2e552143671ca41862
tamim008bd <tamim008bd@gmail.com>
2022-01-09 16:53:28 +0600

    implement project side bar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MainContent from "./components/main-content";
 import Sidebar from "./components/sidebar";
 import { Row,Col, Container } from "reactstrap";
 
-import Polls from "./data/polls";
+import Polls from "./data/polls.js";
 import shortid from "shortid";
 class App extends React.Component{
   state={
@@ -13,7 +13,7 @@ class App extends React.Component{
   }
 
   componentDidMount(){
-    this.setState({Polls})
+    this.setState({polls:Polls})
     console.log(this.state.polls)
   }
 
@@ -48,16 +48,19 @@ class App extends React.Component{
     const poll = this.state.polls.find(p=>p.id ===pollId)
     this.state({selectedPoll:poll})
   }
+
+  handleSearch = searchTerm =>{}
+
   render(){
     return(
       <div>
           <Container>
             <Row className="my-5">
                 <Col md={4}>
-                  <Sidebar/>
+                  <Sidebar  polls={this.state.polls} searchTerm={this.state.searchTerm} handleSearch={this.handleSearch} selectPoll={this.selectPoll}/>
                 </Col>
                 <Col md={8}>
-                  <MainContent />
+                  <MainContent  />
                 </Col>
             </Row>
           </Container>
--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { Button,Input,Modal,ModalHeader,ModalBody } from "reactstrap";
+
+import PollList from "./poll-list";
 
 class Sidebar extends React.Component{
+    state={openModal:false}
+    toggleModal = () =>{
+        this.setState({openModal:!this.state.openModal})
+    }
     render(){
         return(
-            <h1>Sidebar component</h1>
+            <div style={{background: '#efefef',padding: '10px'}}>
+                <div className="d-flex mb-5">
+                    <Input 
+                        type="search"
+                        placeholder="Search"
+                        value={this.props.searchTerm}
+                        onChange={e=>this.props.hangleSearch(e.target.value)}
+                    />
+                    <Button color='success' className="ms-2" onClick={this.toggleModal}>New</Button>
+                </div>
+                <h3>List of Polls</h3>
+                <hr />
+                <PollList 
+                    polls = {this.props.polls}
+                    selectPoll = {this.props.selectPoll}
+                />
+                <Modal
+                    isOpen={this.state.openModal}
+                    toggle={this.toggleModal}
+                    unmountOnClose={true}
+                >
+                    <ModalHeader toggle={this.toggleModal}>
+                        Create A New Poll
+                    </ModalHeader>
+                    <ModalBody>This is the modal body</ModalBody>
+                </Modal>
+            </div>
+
         )
     }
 }
